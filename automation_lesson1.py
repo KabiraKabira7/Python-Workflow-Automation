49240bd95c1de1ff8b84a8490723d8b3439be18f
Tim Kabira <timothykabira@gmail.com>
2026-08-17 11:18:53 +0300

    initial commit on automation

---
+++ automation_lesson1.py
@@ -0,0 +1,3 @@
+customer_ages = [20,30,75,40,50,25,35,40,50,60]
+average = sum(customer_ages)/len(customer_ages)
+print(average)
---
+++ automation_lesson2.py
@@ -0,0 +1,12 @@
+customer_ages = [22, 25, 31, 28, 35]
+total_age = sum(customer_ages)
+number_of_customers = len(customer_ages)
+Youngest_customer = min(customer_ages)
+oldest_customer = max(customer_ages)
+average_age =total_age/number_of_customers
+
+print('Total age:',total_age)
+print('Number of customers:',number_of_customers)
+print('Average age:',average_age)
+print('Youngest customer:',Youngest_customer )
+print('Oldest customer:',oldest_customer)
